772fdaf76b2b28b955698d7d7972e3d84596ec41
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-12-26 00:16:25 +0100

    Update Immutable.sol

--- Immutable.sol
+++ Immutable.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.3;
 
+contract Immutable{
+    address public owner = msg.sender;
+
+    uint public x ; 
+    function foo() external {
+        require(msg.sender == owner);
+        x +=1;
+    }
+}
