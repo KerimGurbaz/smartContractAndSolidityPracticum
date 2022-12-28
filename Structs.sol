a0f377764a7accda6739228db2f84b75f18e07c7
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-12-28 23:13:09 +0100

    Update Structs.sol

--- Structs.sol
+++ Structs.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.3;
+
+
+contract Structs{
+    struct Car {
+        string model;
+        uint year;
+        address owner;
+
+    }
+
+    Car public car;
+    Car[] public cars;
+    mapping(address => Car[]) public carsByOwner;
+
+    functions examples() external {
+        Car memory toyota = Car('Toyota', 1990, msg.sender);
+        Car memory lambo = Car({model:'Lamborgini', year:1980, owner:msg.sender});
+        Car memory tesla;
+        tesla.model = 'Tesla';
+        tesla.year = 2010;
+        tesla.owner = msg.sender;
+    }
+}
