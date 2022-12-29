46b22a94acaba8ac2e6e39c89a5fdbfe685524cf
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-12-29 13:53:00 +0100

    Update FunctionModifier.sol

--- FunctionModifier.sol
+++ FunctionModifier.sol
@@ -11,6 +11,11 @@ contract FunctionModifier{
     function setPause(bool _paused) external{
         paused = _paused;
     }
+    
+     modifier whenNotPaused(){
+     require(!paused, 'paused');
+     _;
+    }
 
     function inc() external {
     require(!paused, 'paused');
@@ -21,5 +26,30 @@ contract FunctionModifier{
       require(!paused, 'paused');
       count -=1
   }
+  
+  modifier cap(uint _x){
+      require(_x > 100, 'x>=100');
+      _;
+  }
+
+  function incBy(uint _x) external whenNotPaused cap(_x){
+
+      count += _x;
+  }
+
+  modifier sandwich(){
+      // code here
+      count += 10;
+      _;
+      
+      // more code here
+      count *= 2;
+  }
+
+  function foo() external sandwich{
+
+      count +=1;
+  }
+
 
 }
