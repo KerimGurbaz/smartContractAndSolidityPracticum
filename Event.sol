05cba1bdc941f52d6df37f2e05115d2f8dab4e7a
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-12-29 19:53:10 +0100

    Update Event.sol

--- Event.sol
+++ Event.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.3;
 
+contract Event{
+    event Log(string message, uint val);
+    event indexedLog(address indexed sender, uint val);
+
+    function example() external{
+        emit Log('foo', 1234);
+        emit indexedLog(msg.sender, 789);
+    }
+
+    event Message(address indexed _from, address indexed _to, string message);
+
+    function sendMessage(address _to, string calldata message) external {
+        emit Message (msg.sender, _to, message);
+    }
+}
