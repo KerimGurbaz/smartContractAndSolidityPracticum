825df9b86c50fbfa9eb9f545355fe72307cef41f
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-12-29 13:37:39 +0100

    Update FunctionModifier.sol

--- FunctionModifier.sol
+++ FunctionModifier.sol
@@ -1,1 +1,25 @@
 
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.7;
+
+
+contract FunctionModifier{
+    bool public paused;
+    uint public count;
+
+    function setPause(bool _paused) external{
+        paused = _paused;
+    }
+
+    function inc() external {
+    require(!paused, 'paused');
+    count +=1
+  }
+
+  function dec() external {
+      require(!paused, 'paused');
+      count -=1
+  }
+
+}
