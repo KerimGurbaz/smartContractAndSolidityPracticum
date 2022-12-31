63651ea88436a0dabab87760d578c90f36d8252c
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-01 00:14:18 +0100

    Update DataLocations.sol

--- DataLocations.sol
+++ DataLocations.sol
@@ -12,5 +12,16 @@ contract DataLocations{
     function examples() external {
         myStructs[msg.sender] = MyStruct({foo:123, text:'bar'});
         MyStruct storage myStructs = myStructs[msg.sender];
+        
+         myStructs.text = "foo";
+
+        MyStruct memory readOnly = myStructs[msg.sender];
+        readOnly.foo =456;
+
+        _internal(y);
+
+        uint memory memArr = new uint[](3);
+        memArr[0] = 234;
+        return memArr;
     }
 }
