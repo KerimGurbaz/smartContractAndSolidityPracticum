58907082b417db968a3b0ea08ca1459bd229cd48
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-01 00:01:08 +0100

    Create DataLocations.sol

---
+++ DataLocations.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+contract DataLocations{
+    struct MyStruct{
+        uint foo;
+        string text;
+    }
+
+    mapping(address => MyStruct) public myStructs;
+
+    function examples() external {
+        myStructs[msg.sender] = MyStruct({foo:123, text:'bar'});
+        MyStruct storage myStructs = myStructs[msg.sender];
+    }
+}
