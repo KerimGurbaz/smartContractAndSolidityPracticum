4391e25cc0ff1c15f2ce9d032b188bb45514493d
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-12-31 23:46:47 +0100

    Update Library.sol

--- Library.sol
+++ Library.sol
@@ -9,6 +9,27 @@ library Math{
 }
 contract Test{
     function testMax(uint x, uint y) external pure returns(uint){
-        return 0;
+       return Math.max(x,y);
+    }
+}
+
+library ArrayLib{
+    function find(uint[] storage arr, uint x) internal view returns(uint){
+        for(uint i = 0; i < arr.length; i++ ){
+            if(arr[i] == x){
+                return i;
+            } 
+        }
+        revert ("not found");
+    }
+}
+
+contract TestArray{
+    using ArrayLib for uint[];
+    uint[] public arr = [3,2,1];
+    function testFind() external view returns (uint i){
+        // return ArrayLib.find(arr, 2);
+        return arr.find(2);
+
     }
 }
