267afac444fba51a9c7fe8999ee1aab4f1f5fe4d
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2022-12-31 22:52:13 +0100

    Update Errors.sol

--- Errors.sol
+++ Errors.sol
@@ -1,1 +1,11 @@
 
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+
+contract Error{
+    function testRequire(uint _i) public pure {
+        require(_i <=10, 'i > 10');
+        // code
+    }
+}
