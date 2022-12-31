63431ebe968f2a07d11cd2d7f907c62c451dea01
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-01 00:17:52 +0100

    Update DataLocations.sol

--- DataLocations.sol
+++ DataLocations.sol
@@ -9,19 +9,22 @@ contract DataLocations{
 
     mapping(address => MyStruct) public myStructs;
 
-    function examples() external {
+    function examples(uint[] calldata y, string calldata s) external  returns(uint[] memory) {
         myStructs[msg.sender] = MyStruct({foo:123, text:'bar'});
         MyStruct storage myStructs = myStructs[msg.sender];
-        
-         myStructs.text = "foo";
+        myStructs.text = "foo";
 
         MyStruct memory readOnly = myStructs[msg.sender];
         readOnly.foo =456;
 
         _internal(y);
 
-        uint memory memArr = new uint[](3);
+        uint[] memory memArr = new uint[](3);
         memArr[0] = 234;
         return memArr;
+    }
+
+    function _internal(uint[] memory y) private{
+        uint x = y[0];
     }
 }
