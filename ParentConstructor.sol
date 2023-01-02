33af9a9a2d20fe7f26a4ff2fa7d0535dab8d3a90
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-02 21:52:02 +0100

    Update ParentConstructor.sol

--- ParentConstructor.sol
+++ ParentConstructor.sol
@@ -3,3 +3,37 @@ pragma solidity 0.8.7;
 
 // 2 ways to call parent constructors
 //Order of initialilization
+
+
+contract S{
+    string public name;
+
+    constructor(string memory _name){
+        name =_name;
+    }
+}
+
+contract T{
+    string public text;
+
+    constructor (string memory _text){
+        text = _text;
+    }
+}
+
+contract U is S("s"),T("t"){
+
+}
+
+
+contract V is S, T{
+    constructor(string memory _name, string memory _text) S(_name) T(_text){
+
+    }
+}
+
+contract VV is S('s'), T{
+    constructor(string memory _text) T(_text){
+
+    }
+}
