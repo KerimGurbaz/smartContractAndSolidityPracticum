cc7d3153df0175884dce0cc336be1166bb03f8fc
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-17 15:36:16 +0100

    Update Sig.sol

--- Sig.sol
+++ Sig.sol
@@ -22,4 +22,18 @@ contract VerifySig{
     function getEthSignedMessageHash(bytes32 _messageHash) public pure returns(bytes32){
         return keccak256(abi.encodePacked(_messageHash));
     }
+    
+        function getEthSignedMessageHash(bytes32 _messageHash) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(
+            "\x19Ethereum Signed Message:\32",
+            _messageHash
+            ));
+    }
+
+    function recover(bytes32 _ethSignedMessageHash, bytes memoryy _sig)
+    public pure returns (address)
+    {
+        (bytes32 r, bytes32 s, uint8 v) = _split(_sig);
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
 }
