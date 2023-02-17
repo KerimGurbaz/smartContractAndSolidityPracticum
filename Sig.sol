bb959dd596913dca35523834df185439fab87f06
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-17 15:01:32 +0100

    Create Sig.sol

---
+++ Sig.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.16; 
+
+
+contract VerifySig{
+    function verify(address _signer, string memory _message, bytes memory _sig)
+    external pure returns (bool)
+    {
+        
+    }
+}
