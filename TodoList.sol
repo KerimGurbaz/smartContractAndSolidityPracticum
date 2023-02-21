5cdb5620d2a5a05001489973bc625bde5317a81a
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-21 10:49:48 +0100

    Update TodoList.sol

--- TodoList.sol
+++ TodoList.sol
@@ -9,9 +9,25 @@ contract TodoList{
 
     Todo[] public todos;
 
-    function create() external {}
-    
-    function updateText() external {}
+     Todo[] public todos;
 
-    function get() external view {}
+    function create(string calldata _text) external {
+        todos.push(Todo({
+            text : _text,
+
+            completed: false
+        }));
+    }
+    
+    function updateText(uint _index, string calldata _text) external {
+        todos[_index].text = _text;
+
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    function get(uint _index) external view returns(string memory, bool){
+        Todo memory todo = todos[_index];
+        return (todo.text, todo.completed);
+    }
 }
