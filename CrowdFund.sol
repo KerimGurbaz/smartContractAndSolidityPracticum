b32d08c74d9b3dfbfa209c18a08e838daaa2e5f4
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-22 22:25:56 +0100

    Update CrowdFund.sol

--- CrowdFund.sol
+++ CrowdFund.sol
@@ -6,6 +6,16 @@ import './IERC20.sol';
 
 contract CrowdFund{
 
+contract CrowdFund{
+    struct Campaign{
+        address creator;
+        uint goal;
+        uint32 startAt;
+        uint32 endAt;
+        bool claimed;
+    }
+
+
     function launch(
         uint _goal,
         uint32 _startAt,
