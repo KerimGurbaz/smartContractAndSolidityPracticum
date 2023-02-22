c0b2e0cb451370c9004b7f7c5bc3eca3d64b1235
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-02-22 22:17:03 +0100

    Create CrowdFund.sol

---
+++ CrowdFund.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+
+import './IERC20.sol';
+
+contract CrowdFund{
+
+    function launch(
+        uint _goal,
+        uint32 _startAt,
+        uint32 _endAt
+    ) external{
+        
+    }
+}
