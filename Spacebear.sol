5e7b97e438bee3bba3dca363c1cb0a4467a6f17d
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-03 22:26:20 +0100

    Update Spacebear.sol

--- Spacebear.sol
+++ Spacebear.sol
@@ -6,3 +6,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Spacebear is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Spacebear", "SBR") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
+    }
