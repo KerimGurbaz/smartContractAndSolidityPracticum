4d82b126ccaa4d26d169f268de963d6a44e3d325
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-04 23:59:22 +0100

    Update SendTransfer.sol

--- SendTransfer.sol
+++ SendTransfer.sol
@@ -14,3 +14,19 @@ contract Sender{
        require(isSent, "Sending the funds was unsuccessful");
     }
 }
+contract ReceiverNoAction{
+    function balance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    receive() external payable{}
+}
+
+contract ReceiverAction{
+    uint public balanceReceived;
+
+    receive() external payable {
+        balanceReceived += msg.value;
+    }
+
+   
