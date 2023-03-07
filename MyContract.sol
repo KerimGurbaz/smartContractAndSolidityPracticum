4698ea4e1722fe18090cf8be4d6950a3b3a83e47
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-07 22:42:59 +0100

    Update MyContract.sol

--- MyContract.sol
+++ MyContract.sol
@@ -1,1 +1,8 @@
+contract MyContract {
+    string public myString;
 
+    constructor(string memory _myString) {
+        require(bytes(_myString).length > 0, "String cannot be empty");
+        myString = _myString;
+    }
+}
