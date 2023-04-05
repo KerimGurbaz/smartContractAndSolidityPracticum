b95e4c1251009598ae196926e0021b4df82c08e6
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-05 23:48:35 +0200

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.7;
 
 contract HelloWorld {
     string public myString = 'Hello world';
+    
+    function getText() public view returns(string memory){
+    return myString;
+    }
 }
