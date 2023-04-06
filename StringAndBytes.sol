531d5472fbc0d25fc14dc3e92f11de5124591cc0
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-07 00:21:31 +0200

    Update StringAndBytes.sol

--- StringAndBytes.sol
+++ StringAndBytes.sol
@@ -2,3 +2,16 @@
 // SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.7;
+
+contract StringAndBytes{
+    string public greeting = "Greetings";
+
+    bytes public helloText = "Hello, World";
+
+    string public convertedText = string(helloText);
+
+    string private name = "Kerim";
+    string public name2 = "Kerim";
+
+
+}
