6f8010356f0cc56231832f1ae2f74b6d4f7df489
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-06 23:51:59 +0200

    Update Address.sol

--- Address.sol
+++ Address.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.7;
 
 
 contract Address{
-    address private owner =0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address private owner =msg.sender;
 
     function getOwner() public view returns(address){
         return owner;
