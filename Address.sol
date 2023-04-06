3ae6133cf4df21de189ea51e24456fe3b494628c
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-06 23:47:24 +0200

    Update Address.sol

--- Address.sol
+++ Address.sol
@@ -2,3 +2,13 @@
 // SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.7;
+
+
+
+contract Address{
+    address private owner =0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    function getOwner() public view returns(address){
+        return owner;
+    }
+}
