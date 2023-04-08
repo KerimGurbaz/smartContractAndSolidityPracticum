d7dc112be6bd99ada6960138dcd4e75255d60d24
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-08 23:46:39 +0200

    Update Variables2.sol

--- Variables2.sol
+++ Variables2.sol
@@ -4,4 +4,9 @@ pragma solidity ^0.8.0;
 
 contract Variables{
     string public myStateVariable = "state example";
+
+     function add(uint a) public pure returns(uint){
+        uint localUnit = 10;
+        return a + localUnit;
+    }
 }
