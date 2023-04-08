654cfa164f200a26dc7720b54a4676411e7f2b6c
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-09 00:08:33 +0200

    Update Functions.sol

--- Functions.sol
+++ Functions.sol
@@ -5,3 +5,7 @@ pragma solidity ^0.8.0;
  function viewExample() public view returns(string memory){
         return stateString;
     }
+    
+        function add(uint a, uint b) public pure returns(uint){
+        return a + b;
+    }
