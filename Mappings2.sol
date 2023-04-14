d856e5bbec61c8a6158c8c622e9fbec2ed79563a
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-14 23:32:45 +0200

    Update Mappings2.sol

--- Mappings2.sol
+++ Mappings2.sol
@@ -4,5 +4,14 @@ pragma solidity ^0.8.17;
 
 contract Mapping{
     mapping(address => string) public names;
+    
+    
+       function setName(string memory _name) public{
+        names[msg.sender] = _name;
+    }
+
+    function getname()public view returns(string memory){
+        return names[msg.sender];
+    }
 
 }
