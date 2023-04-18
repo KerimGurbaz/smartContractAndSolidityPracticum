3912e1fcd07c638fbefefdbfbca4f082c14848e6
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-18 14:28:57 +0200

    Update Structs2.sol

--- Structs2.sol
+++ Structs2.sol
@@ -8,4 +8,12 @@ contract Structs{
         uint age;
         address wallet;
     }
+    
+      User[] public users;
+
+    function doOperations() external{
+        User memory user1 = User("Joe", 34 , msg.sender);
+        User memory user2 = User({name: "Alice", age:23}, wallet:msg.sender);
+        User memory user3;
+    }
 }
