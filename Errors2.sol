cd89d0cc8550775a96ed5cf6637e3b370638162e
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-19 15:06:20 +0200

    Create Errors2.sol

---
+++ Errors2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+
+contract Error2{
+    uint storedData;
+    function set(uint x)public{
+        require(x !=0, "x cannot be zero"); // require statement to check input
+        storedData = x;
+    }
+
+    
+}
