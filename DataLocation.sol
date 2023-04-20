a5307337056ca5575dccacef017f60491fac20e8
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-20 23:28:18 +0200

    Update DataLocation.sol

--- DataLocation.sol
+++ DataLocation.sol
@@ -6,5 +6,12 @@ contract Example{
     uint storedData;
     string storedString;
 
-    
+      function set(uint x, string calldata s) public{
+        storedData = x; // set value in storage
+        storedString= s; // set string value in storage
+    }
+
+    function get() public view returns(uint, string memory){
+        return (storedData, storedString); //read from storage
+    }
 }
